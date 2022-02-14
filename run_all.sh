be45b0d9c3d44127359704e7aa5f7c8da2e302c2
Zihao <expye@outlook.com>
2022-02-14 23:48:22 +0000

    upd

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for model in GAT GCN HGT RGCN CGAT
 do
-  python example/${model}/${model}.py all 0
+  python examples/${model}/${model}.py all 0
   python scripts/plot.py --model ${model} --time
   python scripts/plot.py --model ${model} --mem
 done
