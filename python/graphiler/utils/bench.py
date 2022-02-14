80e0554c5b330398541218cf0f76368688d281ca
Zihao <expye@outlook.com>
2022-02-14 01:35:55 +0000

    handle OOM

--- python/graphiler/utils/bench.py
+++ python/graphiler/utils/bench.py
@@ -17,22 +17,29 @@ def bench(net, net_params, tag="", nvprof=False, memory=False, steps=1001, prof_
         net(*net_params)
     synchronize()
     reset_peak_memory_stats()
-    if nvprof:
-        profiler.start()
-    start_time = time.time()
-    for i in range(steps):
-        logits = net(*net_params)
-    synchronize()
-    if nvprof:
-        profiler.stop()
-    elapsed_time = (time.time() - start_time) / steps * 1000
-    print("{} elapsed time: {} ms/infer".format(tag, elapsed_time))
-    if prof_df is not None:
-        prof_df[tag, "elapsed_time"] = elapsed_time 
-    if memory:
-        max_mem_consumption = max_memory_allocated() / 1048576
-        print("max memory consumption: {} MB".format(max_mem_consumption))
-        prof_df[tag, "mem"] = max_mem_consumption
+    try:
+        if nvprof:
+            profiler.start()
+        start_time = time.time()
+        for i in range(steps):
+            logits = net(*net_params)
+        synchronize()
+        if nvprof:
+            profiler.stop()
+        elapsed_time = (time.time() - start_time) / steps * 1000
+        print("{} elapsed time: {} ms/infer".format(tag, elapsed_time))
+        if prof_df is not None:
+            prof_df[tag, "elapsed_time"] = elapsed_time 
+        if memory:
+            max_mem_consumption = max_memory_allocated() / 1048576
+            print("max memory consumption: {} MB".format(max_mem_consumption))
+            prof_df[tag, "mem"] = max_mem_consumption
+    except RuntimeError:
+        print("{} OOM".format(tag))
+        return None
+    except BaseException as e:
+        print(e)
+        raise
     return logits
 
 
